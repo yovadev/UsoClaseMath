a78d4bfc29a1c795ebdee08fa06a6483a8571a49
Yova <10067268+yovadev@users.noreply.github.com>
2018-10-22 10:51:37 -0600

    Initial commit

---
+++ src/usoclasemath/UsoClaseMath.java
@@ -0,0 +1,37 @@
+package usoclasemath;
+
+
+public class UsoClaseMath {
+
+
+    public static void main(String[] args) {
+        /** devuelve el valor absoluto **/
+        System.out.println(Math.abs(-20));
+        
+        /** devuelve el valor redondeado **/
+        System.out.println(Math.round(7.8));
+        
+        /** devuelve el valor redondeado hacia arriba **/
+        System.out.println(Math.ceil(7.2));
+        
+        /** devuelve el valor redondeado hacia abajo **/
+        System.out.println(Math.floor(7.8));
+        
+       /** devuelve el menor de 2 numeros **/
+        System.out.println(Math.min(7.7, 8.2));
+        
+        /** devuelve el mayor de 2 numeros **/
+        System.out.println(Math.max(7.7, 8.2));
+        
+        /** devuelve el valor de PI **/
+        System.out.println(Math.PI);
+        
+        /** devuelve la raiz cuadrada **/
+        System.out.println(Math.sqrt(4));
+        
+        /** devuelve la potentencia **/
+        System.out.println(Math.pow(4,2));
+    }
+    
+
+}
